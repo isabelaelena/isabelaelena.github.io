c403af6a7b4683cd8d23733d98c7cb3369d2a461
Abed Fayyad <abed.ef@icloud.com>
2024-05-22 10:53:49 -0400

    Replace files with Svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Isabela Elena Portfolio</title>
+</svelte:head>
+
+<header>
+    <h1>Elena Ng </h1>
+    <div class="topnav">
+        <a class="active" href="#home">Elena Ng</a>
+        <a href="#about">About</a>
+        <a href="#works">Works</a>
+        <a href="#contact">Contact</a>
+        </div>
+</header>
+
+<section>
+    <h2>get to know me</h2>
+</section>
+
+<section id="works">
+    <h2>works / projects </h2>
+    <div class="card">
+        <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+        </div>
+    </div>
+</section>
+
+<section>
+    <h3>have coffee with me</h3>
+</section>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+
+    #works {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+
+    .container {
+        color: red;
+    }
+</style>
