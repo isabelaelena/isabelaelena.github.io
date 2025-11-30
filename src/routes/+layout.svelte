c8b4116638c8241804a393cea4bc132ba68fc12d
Isabela Elena <isabelaelenang@gmail.com>
2025-11-30 18:19:12 -0500

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
     <!-- desktop (equal-width items across the full bar) -->
     <ul class="hidden w-full justify-end space-x-8 md:flex">
       <li class="mr-auto"><a href="/" class="font-bold">Elena Ng</a></li>
-      <li><a href="#works" class="hover:italic">Work</a></li>
+      <!-- <li><a href="#works" class="hover:italic">Work</a></li> -->
       <!-- <li><a href="#about" class="hover:italic">About</a></li> -->
       <li><a href="#contact" class="hover:italic">Contact</a></li>
     </ul>
