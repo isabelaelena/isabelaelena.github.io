255452559d072c24be4b82ff2e3aa7255b5c62d7
Isabela Elena <isabelaelenang@gmail.com>
2025-12-20 15:25:41 -0500

    I deleted the a href on layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
     <!-- desktop (equal-width items across the full bar) -->
     <ul class="hidden w-full justify-end space-x-8 md:flex">
       <li class="mr-auto"><a href="/" class="font-bold">Elena Ng</a></li>
-      <li><a href="#works" class="hover:italic">Work</a></li>
-      <li><a href="#contact" class="hover:italic">Contact</a></li>
+      <li><class="hover:italic">Work</a></li>
+      <li><class="hover:italic">Contact</a></li>
     </ul>
   </div>
 </nav>
